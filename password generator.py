22afca0a8d6c4c7fbbc8f54ece6b13128f587d4e
Priyanshu Patel <priyanshuabhi56@gmail.com>
2025-04-21 19:29:21 +0530

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -1,5 +1,14 @@
-import random
-x=int(input("enter the password length , you want to generate  "))
-if(x<=100):
-    password=random.choice 
-    print("your password is ",password)
+i=0
+while i==0:
+    import random
+    import string
+    x=int(input("enter the password length , you want to generate  "))
+    chars =string.ascii_letters
+    chars +=string.digits
+    chars +=string.punctuation
+    password=""
+    for i in range(x):
+        next_char = random.choice(chars)
+        password +=next_char
+        print("your random password generated is ", password)
+        i +=1      
